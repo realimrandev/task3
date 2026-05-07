6074ccbf3843c245122d24c05801c96e428e5e61
realimrandev <imransabir2005@gmail.com>
2026-05-07 16:37:04 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
+
+export default function App() {
+  return (
+    <>
+      <div data-theme="forest">
+        div className="absola
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/note/:id" element={<NoteDetailPage />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
